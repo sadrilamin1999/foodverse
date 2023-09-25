2292baf4916586688db4cf30e97cd9fd7a001b3b
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-25 23:46:30 +0600

    search query added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./conponents/Navbar";
+import { useRef, useState } from "react";
 import Home from "./conponents/Home";
 import Favourites from "./conponents/Favourites";
 import NotFound from "./conponents/NotFound";
 import Footer from "./conponents/Footer";
 
 const App = () => {
+  const [searchQuery, setSearchQuery] = useState();
+  const inputField = useRef(null);
+
+  // search handler
+  const searchHander = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+
+    setSearchQuery("");
+    inputField.current.blur();
+  };
+
   return (
     <>
       <div className="App min-h-screen bg-rose-50 text-lg text-gray-600">
-        <Navbar />
+        <Navbar
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          searchHander={searchHander}
+          inputField={inputField}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favourites" element={<Favourites />} />
--- src/conponents/Navbar.js
+++ src/conponents/Navbar.js
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ searchHander, searchQuery, setSearchQuery, inputField }) => {
   // nav active styles
   const navActive = ({ isActive }) => {
     return {
       color: isActive ? "#06b6d4" : null,
     };
   };
+
   return (
     <div className="navbar py-8 flex flex-col lg:flex-row justify-between items-center gap-5 lg:gap-0 container mx-auto">
       <h2 className="logo text-2xl font-bold lowercase italic">
         food<sapn className="text-rose-500 ">verse</sapn>
       </h2>
-      <form className="search-bar">
+      <form className="search-bar" onSubmit={searchHander}>
         <input
+          ref={inputField}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           type="search"
           placeholder="Search recipe..."
           className=" bg-white/75 p-3 px-8 lg:w-96 rounded-full outline-none shadow-lg shadow-rose-100 focus:shadow-rose-200 duration-300"
