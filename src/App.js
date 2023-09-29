2a1a4f928a168378878654e4a7f91f680765fb2b
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-09-29 20:42:26 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./conponents/Navbar";
 import { useRef, useState } from "react";
+import Navbar from "./conponents/Navbar";
 import Home from "./conponents/Home";
 import Favourites from "./conponents/Favourites";
 import NotFound from "./conponents/NotFound";
 import Footer from "./conponents/Footer";
-import { useFetch } from "./conponents/hooks/useFetch";
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const inputField = useRef(null);
-  const { data: recipes, loading, error } = useFetch(searchQuery);
 
   // search handler
   const searchHander = (e) => {
     e.preventDefault();
 
-    const searchValue = e.target.search.value;
-    setSearchQuery(searchValue);
-
-    console.log(recipes);
+    getData(searchQuery);
 
     setSearchQuery("");
     inputField.current.blur();
+  };
+
+  const getData = async (searchQuery) => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/search?q=${searchQuery}`
+      );
+      if (!res.ok) throw new Error("No recipe found!");
+      const data = await res.json();
+      console.log(data);
+      setRecipes(data.recipes);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
@@ -35,7 +50,10 @@ const App = () => {
           searchHander={searchHander}
         />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home recipes={recipes} loading={loading} error={error} />}
+          />
           <Route path="/favourites" element={<Favourites />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
